4884621bba6cce4bb5a42228b0b3957fbf0a60fd
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-12-12 03:13:59 +0600

    submission ready

--- 1605062.py
+++ 1605062.py
@@ -1,10 +1,10 @@
-from os import XATTR_SIZE_MAX
 import pandas as pd
 import numpy as np
 import logging
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import confusion_matrix
 
 # logging initialization
 
@@ -21,6 +21,10 @@ logger.addHandler(stream_handler)
 regression_logger = logging.getLogger("Linear_Regression")
 regression_logger.setLevel(logging.ERROR)
 regression_logger.addHandler(stream_handler)
+
+# global variables
+RANDOM_SEED = 7
+TEST_DATA_FRACTION = 0.2
 
 
 class Logistic_Regression:
@@ -184,8 +188,7 @@ def adaboost(x, y, L, k=5):
     adaboost_logger.info(index_array)
     adaboost_logger.info(prob_array)
 
-    seed_value = 5
-    np.random.seed(seed_value)
+    np.random.seed(RANDOM_SEED)
 
     for i in range(k):
         sampled_index = np.random.choice(index_array, size=example_count, p=prob_array)
@@ -238,6 +241,24 @@ def adaboost(x, y, L, k=5):
     return Weighted_Majority(hypothesis_group, hypothesis_weights)
 
 
+def performance_measure(y_predicted, y_real):
+
+    accuracy = (y_real == y_predicted).mean()
+    logger.warning(f"accuracy: {accuracy}")
+
+    tn, fp, fn, tp = confusion_matrix(y_real, y_predicted).ravel()
+    logger.warning(f"tp:{tp},        fn:{fn}")
+    logger.warning(f"fp:{fp},        tn:{tn}")
+
+    logger.warning(
+        f"True positive rate (sensitivity, recall, hit rate): {(tp/(tp+fn))}"
+    )
+    logger.warning(f"True negative rate (specificity): {(tn/(tn+fp))}")
+    logger.warning(f"Positive predictive value (precision): {(tp/(tp+fp))}")
+    logger.warning(f"False discovery rate: {(fp/(tp+fp))}")
+    logger.warning(f"F1 score: {(2*tp/(2*tp+fp+fn))}")
+
+
 def dataset_1():
 
     df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
@@ -287,7 +308,7 @@ def dataset_1():
         df[total_charges.isnull()][["customerID", "TotalCharges", "MonthlyCharges"]]
     )
 
-    # converting Y/Output value from yes/no to 1/-1
+    # converting  from yes/no to 1/0
     logger.info(df.Churn.head())
     logger.info(df.Churn.value_counts())
     df.Churn = df["Churn"].replace(to_replace="Yes", value=1)
@@ -304,20 +325,16 @@ def dataset_1():
     logger.info(df[["customerID"] + numerical_columns])
 
     # using same random_seed variable for reproducible result
-    random_seed = 7
-    test_data_fraction = 0.2
 
     # splitting data for test and training
     training_data, testing_data = train_test_split(
-        df, test_size=test_data_fraction, random_state=random_seed
+        df, test_size=TEST_DATA_FRACTION, random_state=RANDOM_SEED
     )
-    
 
     output_of_training_data = training_data.Churn.values
     del training_data["Churn"]
     output_of_testing_data = testing_data.Churn.values
     del testing_data["Churn"]
-    
 
     # doing catagorical value encoding using skitlearn's DictVectorizer class
     training_data_dict = training_data[categorical_columns + numerical_columns].to_dict(
@@ -330,7 +347,6 @@ def dataset_1():
     logger.info(len(dictionary_vectorizer.get_feature_names_out()))
     logger.info(training_data_feature_matrix_x[0])
 
-  
     logger.info(type(training_data_feature_matrix_x))
     logger.info(type(output_of_training_data))
     x = training_data_feature_matrix_x
@@ -346,18 +362,15 @@ def dataset_1():
 
     logger.info(np.bincount(churn))
     logger.info(np.bincount(y))
-    result = (y == churn).mean()
-    logger.info(result)
+    performance_measure(churn, y)
 
     # doing catagorical value encoding using skitlearn's DictVectorizer class
-    testing_data_dict = testing_data[
-        categorical_columns + numerical_columns
-    ].to_dict(orient="records")
+    testing_data_dict = testing_data[categorical_columns + numerical_columns].to_dict(
+        orient="records"
+    )
     dictionary_vectorizer = DictVectorizer(sparse=False)
     dictionary_vectorizer.fit(testing_data_dict)
-    testing_data_feature_matrix_x = dictionary_vectorizer.transform(
-        testing_data_dict
-    )
+    testing_data_feature_matrix_x = dictionary_vectorizer.transform(testing_data_dict)
     logger.info(dictionary_vectorizer.get_feature_names_out())
     logger.info(len(dictionary_vectorizer.get_feature_names_out()))
     logger.info(training_data_feature_matrix_x[0])
@@ -375,19 +388,18 @@ def dataset_1():
 
     logger.info(np.bincount(churn))
     logger.info(np.bincount(y_test))
-    result = (y_test == churn).mean()
-    logger.info(result)
+    performance_measure(churn, y_test)
 
-
-    majority_model = adaboost(x, y, Logistic_Regression, k=15)
+    majority_model = adaboost(x, y, Logistic_Regression, k=20)
     churn = majority_model.predict(x)
 
     logger.info(churn)
 
     logger.info(np.bincount(churn))
     logger.info(np.bincount(y))
+
     result = (y == churn).mean()
-    logger.info(result)
+    logger.warning(f"adaboost training data result: {result}")
 
     churn = majority_model.predict(x_test)
 
@@ -396,8 +408,258 @@ def dataset_1():
     logger.info(np.bincount(churn))
     logger.info(np.bincount(y_test))
     result = (y_test == churn).mean()
-    logger.info(result)
+    logger.warning(f"adaboost test data result: {result}")
 
 
-dataset_1()
+def dataset_3():
+    df = pd.read_csv("creditcard.csv")
+    logger.info(df.head())
+    logger.info(df.info())
+    logger.info(df["Class"].value_counts())
 
+    # checking and removing duplicates
+    logger.info(df.duplicated().sum())
+    df.drop_duplicates(inplace=True)
+    logger.info(df.duplicated().sum())
+
+    # as positive data is scarce hence sampling negetive datas
+    # and concatanating two class
+    positive_df = df[df.Class == 1]
+    negetive_df = df[df.Class == 0]
+    sampled_negetive_df = negetive_df.sample(n=10000, random_state=RANDOM_SEED)
+    sampled_df = pd.concat([positive_df, sampled_negetive_df])
+    logger.info(sampled_df.to_numpy().shape)
+
+    # removing unnecessary columns and forming input x and y
+    x = sampled_df.drop(["Class", "Time"], axis=1)
+    y = sampled_df["Class"]
+    logger.info(x.to_numpy().shape)
+    logger.info(y.to_numpy().shape)
+
+    # normalizing numerical attributes
+    for attribute in x.columns:
+        min_value = x[attribute].min()
+        max_value = x[attribute].max()
+        x[attribute] = (x[attribute] - min_value) / (max_value - min_value)
+    logger.info(x)
+    logger.info(y)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=TEST_DATA_FRACTION, random_state=RANDOM_SEED
+    )
+    logger.info(x_train.shape)
+    logger.info(x_test.shape)
+    logger.info(y_train.shape)
+    logger.info(y_test.shape)
+
+    # converting y values to numpy arr
+    # x values gets converted when we create feature matrix
+    y_train = y_train.to_numpy()
+    y_test = y_test.to_numpy()
+
+    # doing catagorical value encoding using skitlearn's DictVectorizer class
+    training_data_dict = x_train.to_dict(orient="records")
+    dictionary_vectorizer = DictVectorizer(sparse=False)
+    dictionary_vectorizer.fit(training_data_dict)
+    training_data_feature_matrix_x = dictionary_vectorizer.transform(training_data_dict)
+    logger.info(dictionary_vectorizer.get_feature_names_out())
+    logger.info(len(dictionary_vectorizer.get_feature_names_out()))
+    logger.info(training_data_feature_matrix_x[0])
+
+    logger.info(type(training_data_feature_matrix_x))
+    logger.info(type(y_train))
+    x_train = training_data_feature_matrix_x
+
+    logger.info(x_train.shape)
+    logger.info(y_train.shape)
+    logger.info(np.bincount(y))
+
+    model = Logistic_Regression(x_train, y_train, alpha=0.0005, max_iterations=1000)
+    logger.info(model.co_eff)
+    prediction = model.predict(x_train)
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_train))
+    performance_measure(prediction, y_train)
+
+    test_data_dict = x_test.to_dict(orient="records")
+    dictionary_vectorizer = DictVectorizer(sparse=False)
+    dictionary_vectorizer.fit(test_data_dict)
+    testing_data_feature_matrix_x = dictionary_vectorizer.transform(test_data_dict)
+    logger.info(dictionary_vectorizer.get_feature_names_out())
+    logger.info(len(dictionary_vectorizer.get_feature_names_out()))
+    logger.info(testing_data_feature_matrix_x[0])
+
+    logger.info(type(testing_data_feature_matrix_x))
+    logger.info(type(y_test))
+    x_test = testing_data_feature_matrix_x
+
+    prediction = model.predict(x_test)
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_test))
+    performance_measure(prediction, y_test)
+
+    majority_model = adaboost(x_train, y_train, Logistic_Regression, k=20)
+    prediction = majority_model.predict(x_train)
+
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_train))
+
+    result = (y_train == prediction).mean()
+    logger.warning(f"adaboost training data result: {result}")
+
+    prediction = majority_model.predict(x_test)
+
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_test))
+    result = (y_test == prediction).mean()
+    logger.warning(f"adaboost test data result: {result}")
+
+
+def dataset_2():
+    df1 = pd.read_csv("adult.data", header=None)
+    df2 = pd.read_csv("adult.test", header=None)
+    # continuous 0,2,4,10,11,12
+    # discrete 1,3,5,6,7,8,9,13
+    # 0 age: continuous.
+    # 1 workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked.
+    # 2 fnlwgt: continuous.
+    # 3 education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool.
+    # 4 education-num: continuous.
+    # 5 marital-status: Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse.
+    # 6 occupation: Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces.
+    # 7 relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried.
+    # 8 race: White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black.
+    # 9 sex: Female, Male.
+    # 10 capital-gain: continuous.
+    # 11 capital-loss: continuous.
+    # 12 hours-per-week: continuous.
+    # 13 native-country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc), India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands.
+    # 14 <=50 or >50
+
+    def clean_dataframe(df):
+        logger.info(df.head())
+        logger.info(df.info())
+        logger.info(df.loc[[0]].to_dict(orient="records"))
+        logger.info(df[df.columns].nunique())
+
+        logger.info(df.duplicated().sum())
+        df.drop_duplicates(inplace=True)
+        logger.info(df.duplicated().sum())
+
+        # we have only one data point for holand-netherlands
+        # but no test data for this so
+        # so our test feature column will have one less column and therefore
+        # won't work properly
+        # taking easy way out by romving this data point
+
+        logger.info(df[df[13] == " Holand-Netherlands"])
+        df = df[df[13] != " Holand-Netherlands"]
+        logger.info(df[df[13] == " Holand-Netherlands"])
+
+        df[14] = df[14].replace(to_replace=" <=50K", value=0)
+        df[14] = df[14].replace(to_replace=" <=50K.", value=0)
+        df[14] = df[14].replace(to_replace=" >50K", value=1)
+        df[14] = df[14].replace(to_replace=" >50K.", value=1)
+        logger.info(df[14])
+        logger.info(df[14].nunique())
+
+        catagorical_columns = [1, 3, 5, 6, 7, 8, 9, 13]
+        numerical_columns = [0, 2, 4, 10, 11, 12]
+
+        for col in catagorical_columns:
+            df[col] = df[col].str.strip()
+            df[col] = df[col].str.lower()
+
+        # replacing question mark with mode of that column
+        logger.info(df[df[1] == "?"])
+        df_1_mode = df[1].mode()[0]
+        df_6_mode = df[6].mode()[0]
+        df_13_mode = df[13].mode()[0]
+        logger.info(df[df[1] == df_1_mode])
+        df[1] = df[1].replace(to_replace="?", value=df_1_mode)
+        df[6] = df[6].replace(to_replace="?", value=df_6_mode)
+        df[13] = df[13].replace(to_replace="?", value=df_13_mode)
+        logger.info(df[df[1] == df_1_mode])
+
+        # normalizing numerical attributes
+        for attribute in numerical_columns:
+            min_value = df[attribute].min()
+            max_value = df[attribute].max()
+            df[attribute] = (df[attribute] - min_value) / (max_value - min_value)
+
+        logger.info(df)
+
+        output_of_training_data = df[14].values
+        del df[14]
+
+        # doing catagorical value encoding using skitlearn's DictVectorizer class
+        training_data_feature_matrix_x = pd.get_dummies(df)
+        logger.info(training_data_feature_matrix_x.shape)
+        logger.info(training_data_feature_matrix_x[0])
+
+        logger.info(type(training_data_feature_matrix_x))
+        logger.info(type(output_of_training_data))
+
+        logger.info(list(training_data_feature_matrix_x.columns))
+        # '13_holand-netherlands'
+        # logger.info(training_data_feature_matrix_x[training_data_feature_matrix_x['13_holand-netherlands']==1])
+
+        return training_data_feature_matrix_x.to_numpy(), output_of_training_data
+
+    x_train, y_train = clean_dataframe(df1)
+    x_test, y_test = clean_dataframe(df2)
+
+    logger.info(x_train.shape)
+    logger.info(y_train.shape)
+    logger.info(np.bincount(y_train))
+
+    model = Logistic_Regression(x_train, y_train)
+    logger.info(model.co_eff)
+    prediction = model.predict(x_train)
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_train))
+    performance_measure(prediction, y_train)
+
+    logger.info(x_test.shape)
+    logger.info(y_test.shape)
+    # logger.info(np.bincount(y_test))
+
+    prediction = model.predict(x_test)
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    # logger.info(np.bincount(y_test))
+    performance_measure(prediction, y_test)
+
+    majority_model = adaboost(x_train, y_train, Logistic_Regression, k=20)
+    prediction = majority_model.predict(x_train)
+
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_train))
+
+    result = (y_train == prediction).mean()
+    logger.warning(f"adaboost training data result: {result}")
+
+    prediction = majority_model.predict(x_test)
+
+    logger.info(prediction)
+
+    logger.info(np.bincount(prediction))
+    logger.info(np.bincount(y_test))
+    result = (y_test == prediction).mean()
+    logger.warning(f"adaboost test data result: {result}")
+
+
+dataset_3()
